21c00274f8ebe9ae85edb0350c1e85b80e93bfe4
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-16 22:19:51 +0200

    adds layout with grids

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import s from './App.module.css';
 import Contacts from './components/Contacts/Contacts';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Phonebook from './components/Phonebook/Phonebook';
 import Filter from './components/Filter';
 
 const App = () => {
   return (
-    <section className={s.phonebookSection}>
-      <Phonebook />
-      <Filter />
-      <Contacts />
-    </section>
+    <div className={s.wrapper}>
+      <Header className={s.header} />
+      <main className={(s.main, s.phonebookSection)}>
+        <Phonebook />
+        <Filter />
+        <Contacts />
+      </main>
+      <Footer className={s.footer} />
+    </div>
   );
 };
 export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { format } from 'date-fns';
+import s from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={s.footerBox}>
+      <p>Phonebook © 2021 - {format(new Date(), 'yyyy')} All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
