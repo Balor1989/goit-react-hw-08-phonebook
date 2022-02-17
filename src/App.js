b30dee16cb6ce4ebca4500b53148f6b670178d0a
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-17 15:05:08 +0200

    adds  reg pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import s from './App.module.css';
-import Contacts from './components/Contacts/Contacts';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Phonebook from './components/Phonebook/Phonebook';
-import Filter from './components/Filter';
+import PhonebookPage from './views/PhonebookPage/PhonebookPage';
+import SignInPage from './views/SignInPage/SignInPage';
+import SignUpPage from './views/SignUpPage/SignUpPage';
 
 const App = () => {
   return (
     <div className={s.wrapper}>
       <Header className={s.header} />
       <main className={(s.main, s.phonebookSection)}>
-        <Phonebook />
-        <Filter />
-        <Contacts />
+        <PhonebookPage />
+        <SignInPage />
+        <SignUpPage />
       </main>
       <Footer className={s.footer} />
     </div>
