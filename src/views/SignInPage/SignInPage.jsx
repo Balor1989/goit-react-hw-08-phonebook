1fc63f372f5156ca8b3ad1125c7a1b90db0e7b8d
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-17 17:00:01 +0200

    adds login page

--- src/views/SignInPage/SignInPage.jsx
+++ src/views/SignInPage/SignInPage.jsx
@@ -1,7 +1,43 @@
+import s from './SignInPage.module.css';
+
+const formSubmit = () => {
+  console.log('submit');
+};
+
 export default function SignInPage() {
   return (
     <section>
-      <h1>Sign In</h1>
+      <form className={s.phonebookForm} onSubmit={formSubmit}>
+        <div className={s.inputBox}>
+          <label className={s.label}>Login</label>
+          <input
+            className={s.input}
+            // value={name}
+            // onChange={handleChange}
+            type="text"
+            name="login"
+            pattern="^[a-zA-Zа-яА-Я]+((['-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа и тире. Например Adrian,Castelmore, d'Artagnan и т. п."
+            required
+          />
+        </div>
+        <div className={s.inputBox}>
+          <label className={s.label}>Password</label>
+          <input
+            className={s.input}
+            // value={phone}
+            // onChange={handleChange}
+            type="text"
+            name="password"
+            required
+          />
+        </div>
+        <div className={s.buttonBox}>
+          <button className={s.deleteButton} type="submit">
+            Sign-in
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
