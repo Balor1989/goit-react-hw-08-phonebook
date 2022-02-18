8b508c54bafaecf97154e42165451f9ba18f4554
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-18 14:51:45 +0200

    adds slice

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -1,5 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 const initState = {
   token: null,
   isLoggedIn: false,
   user: { name: null, email: null },
 };
+
+const authSlice = createSlice({
+  name: 'auth',
+  initState,
+  extraReducers: {},
+});
+
+export default authSlice.reducer;
